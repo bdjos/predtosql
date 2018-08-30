2a5685d477ba79f2ae3289c49c94f56fea9014b9
bdjos <42777251+bdjos@users.noreply.github.com>
2018-08-30 11:20:28 -0400

    Add files via upload

---
+++ dbtest.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 30 11:11:56 2018
+
+@author: BJoseph
+"""
+import pandasdb
+
+db_name = 'bjos'
+table_name = 'forecast'
+
+db = pandasdb.pandasdb(db_name, table_name)
+df = db.pd_from_db()
